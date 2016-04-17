039e6d8b018cccaf1a9bd1de972fcf11c4f33aab
lankton <lankton@gmail@2015.com>
2016-04-17 16:09:12 +0800

    init local git repo

--- app/src/main/java/cn/lankton/lanflowlayout/MainActivity.java
+++ app/src/main/java/cn/lankton/lanflowlayout/MainActivity.java
@@ -2,12 +2,18 @@ package cn.lankton.lanflowlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        v.setVisibility(View.GONE);
     }
 }
